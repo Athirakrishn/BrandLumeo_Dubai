cf17b74ee4a442d09dce676dc0f6f80b9a36d759
Athirakrishn <athiraramakrishnan41020@gmail.com>
2026-08-22 13:03:19 +0530

    change the image size

--- src/components/ClientMarquee.jsx
+++ src/components/ClientMarquee.jsx
@@ -1,4 +1,10 @@
+import Image from 'next/image';
 import { clientLogos } from '@/data/siteData';
+
+// Swap .png/.jpg/.jpeg extensions for the pre-generated .webp version
+function toWebp(src) {
+  return src ? src.replace(/\.(png|jpe?g)$/i, '.webp') : src;
+}
 
 export default function ClientMarquee() {
   const items = [...clientLogos, ...clientLogos];
@@ -10,11 +16,14 @@ export default function ClientMarquee() {
       <div className="clients__marquee">
         <div className="clients__track">
           {items.map((client, i) => (
-            <img
+            <Image
               key={`${client.name}-${i}`}
-              src={client.src}
+              src={toWebp(client.src)}
               alt={`${client.name} logo`}
               className="clients__logo-img"
+              width={140}
+              height={56}
+              style={{ objectFit: 'contain', width: 'auto', height: '100%' }}
               loading="lazy"
             />
           ))}
